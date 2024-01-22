bb963acf36cc9f842493ecd458923a0891083172
Olha <o.adamova82@gmail.com>
2024-01-22 12:08:13 +0100

    responsed changes

--- tests/api/test_pet_get_status.py
+++ tests/api/test_pet_get_status.py
@@ -7,12 +7,12 @@ def test_get_pet_by_status():
             url="https://petstore.swagger.io/v2/pet/findByStatus?",
             params={"status": status},
         )
-        status1 = "available"
-        status2 = "pending"
-        status3 = "sold"
-        assert status1 == "available" and status2 == "pending" and status3 == "sold"
 
         assert response.status_code == 200
 
         pet_list = response.json()
         assert len(pet_list) > 0
+
+        for pet in pet_list:
+            assert pet["status"] == status
+
